0ae102d7fcbe2fa84da5f4c060356fd0bc1b87a9
Jay002 <jayesh2598@gmail.com>
2020-10-19 21:44:08 +0530

    Added: Feature for creating a new contact

--- src/main/java/com/addressBook/controller/AddressBookMain.java
+++ src/main/java/com/addressBook/controller/AddressBookMain.java
@@ -1,10 +1,35 @@
 package com.addressBook.controller;
 
+import java.util.*;
+
+import com.addressBook.model.Contact;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Welcome to Address Book Program!");
+		System.out.println("You can create a new contact.");
 
+		System.out.println("Enter the first name:");
+		String firstName = sc.nextLine().trim();
+		System.out.println("Enter the last name:");
+		String lastName = sc.nextLine().trim();
+		System.out.println("Enter the address:");
+		String address = sc.nextLine().trim();
+		System.out.println("Enter the city:");
+		String city = sc.nextLine().trim();
+		System.out.println("Enter the state:");
+		String state = sc.nextLine().trim();
+		System.out.println("Enter the ZIP:");
+		int zip = Integer.parseInt(sc.nextLine());
+		System.out.println("Enter the phone number:");
+		long phoneNo = Long.parseLong(sc.nextLine());
+		System.out.println("Enter the email:");
+		String email = sc.next().trim();
+		Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
+		System.out.println("The contact has been successfully created.");
+		sc.close();
 	}
 }
---
+++ src/main/java/com/addressBook/model/Contact.java
@@ -0,0 +1,86 @@
+package com.addressBook.model;
+
+public class Contact {
+	private String firstName, lastName, address, city, state, email;
+	private int zip;
+	private long phoneNo;
+
+	// Constructor
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip, long phoneNo,
+			String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
+	// Getters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	// Setters
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+}
