c1cf6f6095075c62700691ab262bfff3312aa5f8
Jay002 <jayesh2598@gmail.com>
2020-10-19 22:12:16 +0530

    Added: Feature to add multiple contacts in same address book added

--- src/main/java/com/addressBook/controller/AddressBookMain.java
+++ src/main/java/com/addressBook/controller/AddressBookMain.java
@@ -1,24 +1,57 @@
 package com.addressBook.controller;
 
 import com.addressBook.model.AddressBook;
+import com.addressBook.model.AddressBookDictionary;
 import com.addressBook.model.Contact;
 
 import java.util.*;
 
 public class AddressBookMain {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Welcome to Address Book Program!");
 
-		AddressBook book = new AddressBook();
+		AddressBookDictionary dictionary = new AddressBookDictionary();
+		boolean makeNewAddressBook = true;
+		while (makeNewAddressBook) {
+			System.out.println(
+					"Enter your choice:\n1. Create new Address Book\t2. Access existing Address Book\t3. Exit");
+			int choice1 = Integer.parseInt(sc.nextLine());
+
+			switch (choice1) {
+			case 1:
+				System.out.println("Enter the Address Book's name:");
+				String newBookName = sc.nextLine();
+				AddressBook newBook = new AddressBook();
+				editAddressBook(newBook);
+				dictionary.addAddressBook(newBookName, newBook);
+				break;
+			case 2:
+				System.out.println("Enter the Address Book's name:");
+				String accessBookName = sc.nextLine();
+				AddressBook accessBook = dictionary.accessAddressBook(accessBookName);
+				editAddressBook(accessBook);
+				dictionary.addAddressBook(accessBookName, accessBook);
+				break;
+			case 3:
+				System.out.println("Thank you for using the application.");
+				makeNewAddressBook = false;
+				break;
+			}
+		}
+		sc.close();
+	}
+
+	public static void editAddressBook(AddressBook book) {
 		boolean loop = true;
 		while (loop) {
 			System.out.println(
-					"Enter your choice:\n1. Enter a new contact\n2. Edit an existing contact\n3. Delete an existing contact\n4. Exit");
-			int choice = Integer.parseInt(sc.nextLine());
+					"Enter your choice:\n1. Enter a new contact\n2. Edit an existing contact\n3. Delete an existing contact\n4. Exit from this address book");
+			int choice2 = Integer.parseInt(sc.nextLine());
 
-			switch (choice) {
+			switch (choice2) {
 			case 1:
 				System.out.println("Enter the first name:");
 				String firstName = sc.nextLine().trim();
@@ -40,7 +73,6 @@ public class AddressBookMain {
 				book.addContact(createContact);
 				System.out.println("This contact has been successfully created and registered.");
 				break;
-
 			case 2:
 				System.out.println("Enter the first name:");
 				String fName = sc.nextLine().trim();
@@ -48,7 +80,6 @@ public class AddressBookMain {
 				String lName = sc.nextLine().trim();
 				book.editContact(fName, lName);
 				break;
-
 			case 3:
 				System.out.println("Enter the first name:");
 				String FName = sc.nextLine().trim();
@@ -57,11 +88,10 @@ public class AddressBookMain {
 				book.deleteContact(FName, LName);
 				break;
 			case 4:
-				System.out.println("Thank you for using the application.");
+				System.out.println("Exiting from this address book.");
 				loop = false;
 				break;
 			}
 		}
-		sc.close();
 	}
 }
---
+++ src/main/java/com/addressBook/model/AddressBookDictionary.java
@@ -0,0 +1,30 @@
+package com.addressBook.model;
+
+import java.util.HashMap;
+
+public class AddressBookDictionary {
+
+	private HashMap<String, AddressBook> dictionary = new HashMap<>();
+
+	// Getter
+	public HashMap<String, AddressBook> getDictionary() {
+		return dictionary;
+	}
+
+	// Setter
+	public void setDictionary(HashMap<String, AddressBook> dictionary) {
+		this.dictionary = dictionary;
+	}
+
+	// Methods
+	public void addAddressBook(String bookName, AddressBook book) {
+		dictionary.put(bookName, book);
+	}
+
+	public AddressBook accessAddressBook(String bookName) {
+		AddressBook book = dictionary.get(bookName);
+		dictionary.remove(bookName);
+		return book;
+	}
+
+}
